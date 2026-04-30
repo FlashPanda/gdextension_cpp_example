ae8f6517864d5e32a9bbbbb62f867856df8da68c
蔡洲董（冬青） <dongqing.czd@xuelangyun.com>
2026-04-30 18:05:12 +0800

    ..

---
+++ src/render/integrators.h
@@ -0,0 +1,18 @@
+#ifndef GDEXTENSION_CPP_EXAMPLE_INTEGRATORS_H
+#define GDEXTENSION_CPP_EXAMPLE_INTEGRATORS_H
+
+namespace godot_rt
+{
+    class Sampler;
+    class Light;
+    class Ray;
+    class Camera;
+    class Hit;
+    class accel_interface;
+    class Integrator
+    {
+
+    };
+}
+
+#endif //GDEXTENSION_CPP_EXAMPLE_INTEGRATORS_H
