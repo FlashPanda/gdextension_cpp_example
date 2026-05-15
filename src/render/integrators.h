74ae3eec806c2dc54623d60c27ab576e77d5af3e
dongqing <dongqing.czd@xuelangyun.com>
2026-05-15 18:29:22 +0800

    integrator

--- src/render/integrators.h
+++ src/render/integrators.h
@@ -1,6 +1,14 @@
 #ifndef GDEXTENSION_CPP_EXAMPLE_INTEGRATORS_H
 #define GDEXTENSION_CPP_EXAMPLE_INTEGRATORS_H
 
+#include <functional>
+#include <memory>
+
+#include <godot_cpp/templates/vector.hpp>
+#include <godot_cpp/variant/aabb.hpp>
+#include <godot_cpp/variant/string.hpp>
+#include <godot_cpp/core/math_defs.hpp>
+
 namespace godot_rt
 {
     class Sampler;
@@ -8,10 +16,37 @@ namespace godot_rt
     class Ray;
     class Camera;
     class Hit;
-    class accel_interface;
+    class AccelInterface;
+
+    // 积分器基类
     class Integrator
     {
+    public:
+        virtual ~Integrator();
 
+        static std::unique_ptr<Integrator> create(
+            const godot::String& name,
+            Camera* camera,
+            Sampler* sampler,
+            AccelInterface* accel,
+            godot::Vector<Light> lights
+            );
+
+        virtual godot::String to_string() const = 0;
+
+        virtual void Render() = 0;
+
+        bool intersect(const Ray& ray, Hit* hit, real_t t_max = Math_INF) const;
+
+        bool intersect_p(const Ray& ray, real_t t_max = Math_INF) const;
+
+        // 判断两点之间是否未被遮挡
+        bool unoccluded(const Hit& p0, const Hit& p1) const;
+
+        AccelInterface* aggregate;
+        godot::Vector<Light> lights;
+    protected:
+        Integrator(AccelInterface* accel, godot::Vector<Light> lights);
     };
 }
 
